11a3064afab6cc373810149f047dbe4a133b330a
Umputun <umputun@gmail.com>
2018-07-14 12:36:17 -0500

    more tests for custom connections

--- connection_test.go
+++ connection_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/globalsign/mgo"
 	"github.com/globalsign/mgo/bson"
+	"github.com/go-pkgz/mongo/.vendor/github.com/stretchr/testify/require"
 	"github.com/hashicorp/go-multierror"
 	"github.com/stretchr/testify/assert"
 )
@@ -48,6 +49,50 @@ func TestConnection_WithCollection(t *testing.T) {
 	assert.Equal(t, mgo.ErrNotFound, err)
 }
 
+func TestConnection_WithCustomCollection(t *testing.T) {
+	c, err := MakeTestConnection(t)
+	require.NoError(t, err)
+
+	defer RemoveTestCollections(t, c, "coll2", "coll3")
+
+	// write co coll2
+	err = c.WithCustomCollection("coll2", func(coll *mgo.Collection) error {
+		for i := 0; i < 22; i++ {
+			r := testRecord{
+				Symbol: fmt.Sprintf("symb-%02d", i%5),
+				Num:    i,
+			}
+			require.NoError(t, coll.Insert(r))
+		}
+		return nil
+	})
+
+	// write co coll3
+	err = c.WithCustomCollection("coll3", func(coll *mgo.Collection) error {
+		for i := 0; i < 33; i++ {
+			r := testRecord{
+				Symbol: fmt.Sprintf("symb-%02d", i%5),
+				Num:    i,
+			}
+			require.NoError(t, coll.Insert(r))
+		}
+		return nil
+	})
+
+	var res []testRecord
+	err = c.WithCustomCollection("coll2", func(coll *mgo.Collection) error {
+		return coll.Find(nil).All(&res)
+	})
+	assert.Nil(t, err)
+	assert.Equal(t, 22, len(res))
+
+	err = c.WithCustomCollection("coll3", func(coll *mgo.Collection) error {
+		return coll.Find(nil).All(&res)
+	})
+	assert.Nil(t, err)
+	assert.Equal(t, 33, len(res))
+}
+
 func TestConnection_WithCollectionNoDB(t *testing.T) {
 	c, err := write(t)
 	if err != nil {
@@ -71,7 +116,7 @@ func TestConnection_WithDB(t *testing.T) {
 	defer RemoveTestCollection(t, c)
 
 	var res []testRecord
-	err = c.WithCustomDB("test", func(dbase *mgo.Database) error {
+	err = c.WithDB(func(dbase *mgo.Database) error {
 		return dbase.C(c.collection).Find(nil).All(&res)
 	})
 	assert.Nil(t, err)
@@ -79,6 +124,36 @@ func TestConnection_WithDB(t *testing.T) {
 
 	err = c.WithDB(func(dbase *mgo.Database) error {
 		return dbase.C(c.collection).Find(nil).All(&res)
+	})
+	assert.Nil(t, err)
+	assert.Equal(t, 100, len(res))
+}
+
+func TestConnection_WithCustomDB(t *testing.T) {
+	c, err := MakeTestConnection(t)
+	require.NoError(t, err)
+
+	defer func() {
+		err = c.WithCustomDB("test_custom", func(dbase *mgo.Database) error {
+			return dbase.DropDatabase()
+		})
+	}()
+
+	var res []testRecord
+
+	c.WithCustomDbCollection("test_custom", "coll1", func(coll *mgo.Collection) error {
+		for i := 0; i < 100; i++ {
+			r := testRecord{
+				Symbol: fmt.Sprintf("symb-%02d", i%5),
+				Num:    i,
+			}
+			require.NoError(t, coll.Insert(r))
+		}
+		return nil
+	})
+
+	err = c.WithCustomDB("test_custom", func(dbase *mgo.Database) error {
+		return dbase.C("coll1").Find(nil).All(&res)
 	})
 	assert.Nil(t, err)
 	assert.Equal(t, 100, len(res))
