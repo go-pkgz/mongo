5c8554f85c56d9578e9470232b2f27d468560357
Umputun <umputun@gmail.com>
2019-10-06 14:56:42 -0500

    WIP: prepare v2 with the official mongo drvier

--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/globalsign/mgo v0.0.0-20180615134936-113d3961e731
 	github.com/go-pkgz/lgr v0.2.2
-	github.com/stretchr/objx v0.1.1 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/stretchr/testify v1.3.0
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
 
 go 1.13
---
+++ v2/server_test.go
@@ -0,0 +1,123 @@
+package mongo
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func TestServer_NewServerGood(t *testing.T) {
+	mongoURL := os.Getenv("MONGO_TEST") + "/test?debug=true"
+	m, ext, err := Connect(context.Background(), options.Client(), mongoURL)
+	require.NoError(t, err)
+	assert.Equal(t, 0, len(ext), "no extras")
+	assert.NotNil(t, m)
+}
+
+//func TestServer_NewServerAuthSSL(t *testing.T) {
+//	mongoURL := os.Getenv("MONGO_TEST_EXT")
+//	if mongoURL == "" {
+//		t.Skip("skipping test, no MONGO_TEST_EXT")
+//		return
+//	}
+//	m, err := NewServerWithURL(mongoURL, 3*time.Second)
+//	assert.Nil(t, err)
+//	assert.NotNil(t, m)
+//	assert.True(t, m.String() != "")
+//}
+//
+//func TestServer_NewServerBad(t *testing.T) {
+//	_, err := NewServerWithURL("mongodb://127.0.0.3:27017/test", 100*time.Millisecond)
+//	assert.NotNil(t, err)
+//	t.Log(err)
+//
+//	_, err = NewServer(mgo.DialInfo{Addrs: []string{"127.0.0.2"}, Timeout: 100 * time.Millisecond}, ServerParams{})
+//	assert.NotNil(t, err)
+//
+//	_, err = NewServer(mgo.DialInfo{}, ServerParams{})
+//	assert.NotNil(t, err)
+//
+//	_, err = NewServerWithURL("mongodb://mongo:27017/test?blah=xxx", 100*time.Millisecond)
+//	assert.NotNil(t, err)
+//	t.Log(err)
+//}
+//
+//func TestServer_parse(t *testing.T) {
+//	tbl := []struct {
+//		mongoURL string
+//		timeout  time.Duration
+//		params   ServerParams
+//		dial     mgo.DialInfo
+//		isErr    bool
+//	}{
+//		{
+//			"mongodb://127.0.0.3:27017/test", time.Millisecond,
+//			ServerParams{ConsistencyMode: 1},
+//			mgo.DialInfo{Addrs: []string{"127.0.0.3:27017"}, Timeout: 1000000, Database: "test",
+//				ReadPreference: &mgo.ReadPreference{Mode: 2}},
+//			false,
+//		},
+//		{
+//			"mongodb://user:passwd@127.0.0.3:27017/test?ssl=true&authSource=admin", time.Millisecond,
+//			ServerParams{ConsistencyMode: 1, SSL: true},
+//			mgo.DialInfo{Addrs: []string{"127.0.0.3:27017"}, Timeout: 1000000, Database: "test", Source: "admin",
+//				Username: "user", Password: "passwd", ReadPreference: &mgo.ReadPreference{Mode: 2}},
+//			false,
+//		},
+//		{
+//			"mongodb://127.0.0.3", time.Millisecond,
+//			ServerParams{ConsistencyMode: 1, SSL: false},
+//			mgo.DialInfo{Addrs: []string{"127.0.0.3"}, Timeout: 1000000, ReadPreference: &mgo.ReadPreference{Mode: 2}},
+//			false,
+//		},
+//		{
+//			"127.0.0.3", time.Millisecond,
+//			ServerParams{ConsistencyMode: 1, SSL: false},
+//			mgo.DialInfo{Addrs: []string{"127.0.0.3"}, Timeout: 1000000, ReadPreference: &mgo.ReadPreference{Mode: 2}},
+//			false,
+//		},
+//		{
+//			"127.0.0.3?xxx=yyy", time.Millisecond,
+//			ServerParams{}, mgo.DialInfo{},
+//			true,
+//		},
+//	}
+//
+//	for i, tt := range tbl {
+//		dial, params, err := parseURL(tt.mongoURL, tt.timeout)
+//		dial.DialServer = nil
+//		if tt.isErr {
+//			assert.NotNil(t, err, "expect error #%d", i)
+//			t.Logf("dial %+v, params %+v", dial, params)
+//			continue
+//		}
+//		assert.Equal(t, tt.dial, dial, "test #%d", i)
+//		assert.Equal(t, tt.params, params, "test #%d", i)
+//	}
+//}
+//
+//func TestGetMongoURLTesting(t *testing.T) {
+//	keepURL := os.Getenv("MONGO_TEST")
+//	defer os.Setenv("MONGO_TEST", keepURL)
+//
+//	os.Setenv("MONGO_TEST", "mongodb://127.0.0.1:27017/test?debug=true")
+//	url := getMongoURL(t)
+//	assert.Equal(t, os.Getenv("MONGO_TEST"), url)
+//
+//	os.Setenv("MONGO_TEST", "")
+//	url = getMongoURL(t)
+//	assert.Equal(t, "mongodb://mongo:27017", url)
+//}
+//
+//func TestGetMongoURLTestingSkip(t *testing.T) {
+//	keepURL := os.Getenv("MONGO_TEST")
+//	defer os.Setenv("MONGO_TEST", keepURL)
+//
+//	os.Setenv("MONGO_TEST", "skip")
+//	_ = getMongoURL(t)
+//	assert.Fail(t, "should skip")
+//}
